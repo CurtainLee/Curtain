5658ffaf7437fd4bec0f742926df62248008b30c
curtain <zpeng_lee@163.com>
2018-03-03 17:23:50 +0800

    初始化

---
+++ src/main/java/com/curtain/UserCenterApplication.java
@@ -0,0 +1,14 @@
+package com.curtain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 入口函数
+ */
+@SpringBootApplication
+public class UserCenterApplication {
+    public static void main(String[] args){
+        SpringApplication.run(UserCenterApplication.class,args);
+    }
+}
